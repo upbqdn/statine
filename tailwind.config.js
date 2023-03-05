d2a1131ada561e7054543ef659e9b5821ebf5744
Marek <mail@marek.onl>
2023-03-05 17:27:38 +0100

    Update CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,48 @@
 module.exports = {
-  content: [
-    "./layouts/*/*.{html,js}",
-  ],
+  content: ["./layouts/*/*.{html,js}"],
   theme: {
     container: {
       center: true,
       screens: {
-        sm: '45rem',
-        md: '45rem',
-        lg: '45rem',
-        xl: '45rem',
-        '2xl': '45rem',
+        sm: "45rem",
+        md: "45rem",
+        lg: "45rem",
+        xl: "45rem",
+        "2xl": "45rem",
       },
     },
     extend: {
       lineHeight: {
-        'loose': '1.7',
+        loose: "1.8",
       },
       colors: {
-        'tp-black': 'rgba(0,0,0,.8)',
-        'dark': '#1d1f21',
+        "tp-black": "rgba(0,0,0,.8)",
+        dark: "#1d1f21",
       },
       fontFamily: {
-        sans: ["ComputerModernSerif", "BlinkMacSystemFont", "avenir next", "avenir",
-               "segoe ui", "helvetica neue", "helvetica",
-               "Cantarell", "Ubuntu", "roboto", "noto", "arial", "sans-serif"],
+        sans: [
+          "ComputerModernSerif",
+          "BlinkMacSystemFont",
+          "avenir next",
+          "avenir",
+          "segoe ui",
+          "helvetica neue",
+          "helvetica",
+          "Cantarell",
+          "Ubuntu",
+          "roboto",
+          "noto",
+          "arial",
+          "sans-serif",
+        ],
       },
       gridTemplateColumns: {
-        'nav': 'repeat(auto-fill, minmax(80px, 1fr))',
-        'minimal-list-item': '15% 85%',
-        'list-item': '.6fr 3fr auto 1fr',
-      }
-    }
+        nav: "repeat(auto-fill, minmax(80px, 1fr))",
+        "minimal-list-item": "15% 85%",
+        "list-item": ".6fr 3fr auto 1fr",
+      },
+    },
   },
   plugins: [],
-  darkMode: 'class',
-}
+  darkMode: "class",
+};
