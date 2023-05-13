ccb3084ee11992cd962ff1f93a8d193feeb17449
Marek <mail@marek.onl>
2023-05-14 00:23:07 +0200

    Add an inline table of contents (toc)

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,48 +1,81 @@
 function updateMode() {
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        document.documentElement.classList.add('dark');
-        localStorage.remark42_theme = 'dark';
-        if (window.REMARK42) {
-            window.REMARK42.changeTheme("dark")
-        }
-    } else {
-        document.documentElement.classList.remove('dark');
-        localStorage.remark42_theme = 'light';
-        if (window.REMARK42) {
-            window.REMARK42.changeTheme("light")
-        }
+  // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+    localStorage.remark42_theme = "dark";
+    if (window.REMARK42) {
+      window.REMARK42.changeTheme("dark");
     }
+  } else {
+    document.documentElement.classList.remove("dark");
+    localStorage.remark42_theme = "light";
+    if (window.REMARK42) {
+      window.REMARK42.changeTheme("light");
+    }
+  }
 }
 
 function toggleMode() {
-    if ('theme' in localStorage) {
-        if (localStorage.theme === 'dark') {
-            // Whenever the user explicitly chooses light mode
-            localStorage.theme = 'light';
-        }
-        else {
-            // Whenever the user explicitly chooses dark mode
-            localStorage.theme = 'dark';
-        }
+  if ("theme" in localStorage) {
+    if (localStorage.theme === "dark") {
+      // Whenever the user explicitly chooses light mode
+      localStorage.theme = "light";
     } else {
-       if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            localStorage.theme = 'light';
-       } else {
-            localStorage.theme = 'dark';
-       }
+      // Whenever the user explicitly chooses dark mode
+      localStorage.theme = "dark";
     }
-    updateMode()
+  } else {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      localStorage.theme = "light";
+    } else {
+      localStorage.theme = "dark";
+    }
+  }
+  updateMode();
 }
-
-window.onload = updateMode();
 
 function toggleMenu() {
-    let navbar = document.getElementById("navbar-default");
-    if (navbar.classList.contains("hidden")) {
-        navbar.classList.remove("hidden");
-    }
-    else {
-        navbar.classList.add("hidden");
-    }
+  let navbar = document.getElementById("navbar-default");
+  if (navbar.classList.contains("hidden")) {
+    navbar.classList.remove("hidden");
+  } else {
+    navbar.classList.add("hidden");
+  }
 }
+
+function toggleToc() {
+  let toc = document.getElementById("TableOfContents");
+  if (window.getComputedStyle(toc, null).display == "none") {
+    toc.style.display = "block";
+  } else {
+    toc.style.display = "none";
+  }
+}
+
+function handleToc() {
+  if (window.innerWidth < 1400) {
+    toc = document.getElementById("TableOfContents");
+    toc.style.display = "none";
+
+    if (!toc.childNodes.length) {
+      document.getElementById("toc-btn").style.display = "none";
+    }
+  } else {
+    toc.style.display = "block";
+  }
+}
+
+window.onload = function () {
+  updateMode();
+  handleToc();
+};
+
+window.onresize = function () {
+  if (window.innerWidth >= 1400) {
+    toc.style.display = "block";
+  }
+};
