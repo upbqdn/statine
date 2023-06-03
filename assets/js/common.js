32eefc16760364e4e394ab4f3ebac2851349ea46
Marek <mail@marek.onl>
2023-06-03 20:41:49 +0200

    Support dynamic theme switching for plots

--- assets/js/common.js
+++ assets/js/common.js
@@ -6,16 +6,41 @@ function updateMode() {
       window.matchMedia("(prefers-color-scheme: dark)").matches)
   ) {
     document.documentElement.classList.add("dark");
+
+    // Adjust code snippets.
     document.getElementById("code-syntax-theme").href = "/css/syntax-dark.css";
 
+    // Adjust plots.
+    let plots = document.getElementsByClassName("plot");
+    for (var i = plots.length - 1; i >= 0; --i) {
+      if (typeof plots[i].src === "undefined") {
+        continue;
+      }
+      let plot_src = plots[i].src.split(".html");
+      plots[i].src = plot_src[0] + "-dark" + ".html";
+    }
+
+    // Adjust the comments section.
     localStorage.remark42_theme = "dark";
     if (window.REMARK42) {
       window.REMARK42.changeTheme("dark");
     }
   } else {
     document.documentElement.classList.remove("dark");
+
+    // Adjust code snippets.
     document.getElementById("code-syntax-theme").href = "/css/syntax-light.css";
 
+    // Adjust plots.
+    let plots = document.getElementsByClassName("plot");
+    for (var i = plots.length - 1; i >= 0; --i) {
+      if (typeof plots[i].src === "undefined") {
+        continue;
+      }
+      plots[i].src = plots[i].src.replace(/-dark/g, "");
+    }
+
+    // Adjust the comments section.
     localStorage.remark42_theme = "light";
     if (window.REMARK42) {
       window.REMARK42.changeTheme("light");
@@ -63,6 +88,9 @@ function toggleToc() {
 function handleToc() {
   if (window.innerWidth < 1400) {
     toc = document.getElementById("TableOfContents");
+    if (typeof toc === "undefined") {
+      return;
+    }
     toc.style.display = "none";
 
     if (!toc.childNodes.length) {
