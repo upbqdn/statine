2f6db55e6019e5722d2cb2da5bcc5be96c5b5780
Marek <mail@marek.onl>
2023-08-06 01:17:19 +0200

    Reformat `toc.js`

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -40,9 +40,9 @@ function toggleToc() {
           rect.bottom >= 0 &&
           rect.right >= 0 &&
           rect.top <=
-            (window.innerHeight || document.documentElement.clientHeight) &&
+          (window.innerHeight || document.documentElement.clientHeight) &&
           rect.left <=
-            (window.innerWidth || document.documentElement.clientWidth)
+          (window.innerWidth || document.documentElement.clientWidth)
         );
       }
 
